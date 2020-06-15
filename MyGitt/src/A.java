4a153eb731a571a97e5e7f1f56ba3077c3c534dd
suyeon <thdcjsryghl@naver.com>
2020-06-15 23:24:13 +0900

    commit 3

--- MyGitt/src/A.java
+++ MyGitt/src/A.java
@@ -3,6 +3,8 @@ public class A {
 	public static void main(String[] args) {
 		System.out.println("Say like food!!!!");
 		System.out.println("Sayyyyyyy");
+		new B().likefood();
+		new C().dislikefood();
 	}
 
 }
